5f36e851f8892eddcccc317ab4af94417426f89b
Jan Henke <mail@janhenke.dev>
2026-06-08 21:48:10 +0200

    Add generated tests

---
+++ integration-tests/src/test/java/dev/janhenke/quarkus/http/message/signatures/it/QuarkusHttpMessageSignaturesResourceIT.java
@@ -0,0 +1,7 @@
+package dev.janhenke.quarkus.http.message.signatures.it;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class QuarkusHttpMessageSignaturesResourceIT extends QuarkusHttpMessageSignaturesResourceTest
+{}
---
+++ integration-tests/src/test/java/dev/janhenke/quarkus/http/message/signatures/it/QuarkusHttpMessageSignaturesResourceTest.java
@@ -0,0 +1,23 @@
+package dev.janhenke.quarkus.http.message.signatures.it;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+@QuarkusTest
+public class QuarkusHttpMessageSignaturesResourceTest
+{
+
+	@Test
+	public void testHelloEndpoint()
+	{
+		given().when()
+		       .get("/quarkus-http-message-signatures")
+		       .then()
+		       .statusCode(200)
+		       .body(is("Hello quarkus-http-message-signatures"));
+	}
+}
